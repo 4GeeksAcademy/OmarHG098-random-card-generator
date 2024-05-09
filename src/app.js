1aacd0292c73dff595bb7a274ae7e3f175a01371
Omar Hidalgo <omar_ehr@outlook.com>
2024-05-09 03:54:51 +0000

    ADD logic

--- src/app.js
+++ src/app.js
@@ -7,5 +7,53 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("something has changed!");
+
+  const cardNumber = Math.floor(Math.random() * 12);
+  const suitNumber = Math.floor(Math.random() * 4);
+  const cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+  const suits = [
+    {
+      name: "spade",
+      symbol: "♠"
+    },
+
+    {
+      name: "heart",
+      symbol: "♥"
+    },
+    {
+      name: "club",
+      symbol: "♣"
+    },
+    {
+      name: "diamond",
+      symbol: "♦"
+    }
+  ];
+  const pokerCard = document.getElementById("pokerCard");
+  pokerCard.classList.add("col-12", "col-md-4", "col-lg-3");
+
+  const suitElement1 = document.createElement("div");
+  suitElement1.innerHTML = `${suits[suitNumber].symbol}`;
+  suits[suitNumber].name === "heart"
+    ? suitElement1.classList.add("fs-1", "text-start", "mx-3", "text-danger")
+    : suitElement1.classList.add("fs-1", "text-start", "mx-3");
+  pokerCard.appendChild(suitElement1);
+
+  const cardElement = document.createElement("div");
+  cardElement.innerHTML = `${cards[cardNumber]}`;
+  cardElement.classList.add("fs-1", "text-center");
+  pokerCard.appendChild(cardElement);
+
+  const suitElement2 = document.createElement("div");
+  suitElement2.innerHTML = `${suits[suitNumber].symbol}`;
+  suitElement2.classList.add("fs-1", "text-end", "mx-3");
+  suits[suitNumber].name === "heart"
+    ? suitElement2.classList.add("fs-1", "text-start", "mx-3", "text-danger")
+    : suitElement2.classList.add("fs-1", "text-start", "mx-3");
+  pokerCard.appendChild(suitElement2);
+
+  console.log(suits);
+  console.log(cards);
 };
